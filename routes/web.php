0814dc75b76ac89770b4df377fc9e31ff1b9d697
Cristian Giraldo <cgiraldovi@hotmail.com>
2024-05-11 22:14:07 -0500

    Segundo commit
    Se ha creado el modelo de la tabla Products

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
 
 Route::get('/', function () {
     return "hello world";
 });
 
 Route::get('/posts', [ProductController::class, 'index']);
+
+Route::get('/test', function () {
+    $post = new Product;
+    $post->name = "producto1";
+    $post->value = 30;
+    $post -> save();
+    return $post;
+});
